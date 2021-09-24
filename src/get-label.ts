496f72cccfac2f6be4a3a545f9047e122a393efa
fml09 <webh4ckpwn@gmail.com>
2021-09-24 17:54:36 +0900

    temp

---
+++ src/get-label.ts
@@ -0,0 +1,19 @@
+import {getOctokit} from '@actions/github'
+
+export interface Options {
+  octokit: ReturnType<typeof getOctokit>
+
+  owner: string
+  repo: string
+  pull_number: number
+}
+
+export const getLabel = async (options: Options): Promise<string> => {
+  const {octokit, ...rest} = options
+
+  const {
+    data: {labels}
+  } = await octokit.rest.pulls.get({...rest})
+
+  return labels.map(label => label.name).join(', ')
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {getOctokit, context} from '@actions/github'
+import {getLabel} from './get-label'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('token', {required: true})
+    const octokit = getOctokit(token)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const result = await getLabel({
+      octokit,
+      owner: core.getInput('owner') || context.repo.owner,
+      repo: core.getInput('repo') || context.repo.repo,
+      pull_number:
+        Number(core.getInput('pull_number')) ||
+        context.payload.pull_request?.number ||
+        context.issue.number
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('labels', result)
   } catch (error) {
     core.setFailed(error.message)
   }
